eb1742720cc4a4890eac3b38147b0700370b397e
Hc747 <harrisoncole05@gmail.com>
2024-02-16 21:43:54 +1100

    - define `UnaryOperator` implementations: `UnaryPositive` and `UnaryNegative`.
    Signed-off-by: Hc747 <harrisoncole05@gmail.com>

---
+++ src/main/java/com/github/hc747/calculator/token/UnaryNegative.java
@@ -0,0 +1,19 @@
+package com.github.hc747.calculator.token;
+
+import jakarta.annotation.Nonnull;
+
+public record UnaryNegative() implements UnaryOperator {
+
+    public static final Operator OPERATOR = new UnaryNegative();
+
+    @Override
+    public int precedence() {
+        return Precedence.PRECEDENCE_UNARY_OPERATOR;
+    }
+
+    @Nonnull
+    @Override
+    public Value evaluate(@Nonnull Value value) {
+        return value.negate();
+    }
+}
---
+++ src/main/java/com/github/hc747/calculator/token/UnaryPositive.java
@@ -0,0 +1,19 @@
+package com.github.hc747.calculator.token;
+
+import jakarta.annotation.Nonnull;
+
+public record UnaryPositive() implements UnaryOperator {
+
+    public static final Operator OPERATOR = new UnaryPositive();
+
+    @Override
+    public int precedence() {
+        return Precedence.PRECEDENCE_UNARY_OPERATOR;
+    }
+
+    @Nonnull
+    @Override
+    public Value evaluate(@Nonnull Value value) {
+        return value;
+    }
+}
