61efe60efc2fad4a38bcc36dd19b9c55492470a7
Hc747 <harrisoncole05@gmail.com>
2024-02-16 23:24:26 +1100

    - implement test cases for factory methods in `Bracket`.
    Signed-off-by: Hc747 <harrisoncole05@gmail.com>

--- src/test/java/com/github/hc747/calculator/token/TestBracket.java
+++ src/test/java/com/github/hc747/calculator/token/TestBracket.java
@@ -1,21 +1,30 @@
 package com.github.hc747.calculator.token;
 
+import jakarta.annotation.Nonnull;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class TestBracket {
 
-    @Test
-    @DisplayName("Bracket::left returns LeftBracket::BRACKET singleton")
-    public void bracket_left_returns_left_bracket() {
-        assertSame(LeftBracket.BRACKET, Bracket.left());
+    @DisplayName("Bracket factory methods return singleton instances")
+    @ParameterizedTest(name = "Bracket::{0} returns {1}::BRACKET singleton")
+    @MethodSource("brackets")
+    public void bracket_factory_methods_return_singleton_instances(final String method, final String type, final Bracket expected, final Bracket value) {
+        assertSame(expected, value);
     }
 
-    @Test
-    @DisplayName("Bracket::right returns RightBracket::BRACKET singleton")
-    public void bracket_right_returns_right_bracket() {
-        assertSame(RightBracket.BRACKET, Bracket.right());
+    @Nonnull
+    private static Stream<Arguments> brackets() {
+        return Stream.of(
+            arguments("left", "LeftBracket", LeftBracket.BRACKET, Bracket.left()),
+            arguments("right", "RightBracket", RightBracket.BRACKET, Bracket.right())
+        );
     }
 }
